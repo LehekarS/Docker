6c4bdd8008060aa6dc292de17c9f45e2d4245268
LehekarS <117654391+LehekarS@users.noreply.github.com>
2024-12-29 19:18:59 +0530

    Update index.js

--- Dockerfiles/Creating_nodejs_dockerfile/index.js
+++ Dockerfiles/Creating_nodejs_dockerfile/index.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Simple route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
 
